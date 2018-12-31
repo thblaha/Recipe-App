797f0bd107b576878d7add0230d1e1c7f79868ef
Veronica Blaha <veronica.blaha@gmail.com>
2018-12-31 10:43:26 -0500

    Updates

---
+++ precache-manifest.779a77a91b362519be47a53682bb11c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ddf09f14658b2bd7ed7",
+    "url": "/Recipe-App/static/css/main.b8514b54.chunk.css"
+  },
+  {
+    "revision": "3ddf09f14658b2bd7ed7",
+    "url": "/Recipe-App/static/js/main.3ddf09f1.chunk.js"
+  },
+  {
+    "revision": "6f4144efcc91e12183da",
+    "url": "/Recipe-App/static/js/1.6f4144ef.chunk.js"
+  },
+  {
+    "revision": "6e40b99318fefee08dae",
+    "url": "/Recipe-App/static/js/runtime~main.6e40b993.js"
+  },
+  {
+    "revision": "d41aa3ff2c3dcd9377199622c115180b",
+    "url": "/Recipe-App/index.html"
+  }
+];
